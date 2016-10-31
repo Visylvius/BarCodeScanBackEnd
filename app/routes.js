edc5a09651868b36080a9767cd297b8e60f00359
Loren Barrick <lorenbarrick@gmail.com>
2016-10-31 09:23:00 -0700

    add user session

--- app/routes.js
+++ app/routes.js
@@ -41,7 +41,9 @@ module.exports = function(app, passport) {
     // we will want this protected so you have to be logged in to visit
     // we will use route middleware to verify this (the isLoggedIn function)
     app.get('/profile', isLoggedIn, function(req, res) {
-        res.render('profile.ejs');
+      res.render('profile.ejs', {
+            user : req.user // get the user out of session and pass to template
+        });
     });
 
     // =====================================
@@ -55,13 +57,15 @@ module.exports = function(app, passport) {
     app.post('/signup', passport.authenticate('local-signup', {
        successRedirect: '/profile', // redirect to the secure profile section
        failureRedirect: '/signup', // redirect back to the signup page if there is an error
-       failureFlash: true // allow flash messages
+       failureFlash: true, // allow flash messages,
+       session: true
    }));
 
    app.post('/login', passport.authenticate('local-login', {
         successRedirect : '/profile', // redirect to the secure profile section
         failureRedirect : '/login', // redirect back to the signup page if there is an error
-        failureFlash : true // allow flash messages
+        failureFlash : true,
+        session: true// allow flash messages
     }));
 
     app.get('/upccode', function(req, res) {
